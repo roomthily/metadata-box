59827de7f16ab77377943ec6f0df9d480f314ea6
roomthily <soren.scott@nsidc.org>
2014-10-21 13:50:21 -0600

    rolling out xerces

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ class core {
     }
   
     package { 
-      [ "vim", "git-core", "build-essential" ]:
+      [ "vim", "git-core", "build-essential"]:
         ensure => ["installed"],
         require => Exec['apt-update']    
     }
@@ -14,9 +14,7 @@ class core {
 class python {
 
     package { 
-      [ "python", "python-setuptools", "python-dev", "python-pip",
-        "python-matplotlib", "python-imaging", "python-numpy", "python-scipy",
-        "python-software-properties", "idle", "python-qt4", "python-wxgtk2.8"]:
+      [ "python", "python-setuptools", "python-dev", "python-pip"]:
         ensure => ["installed"],
         require => Exec['apt-update']    
     }
@@ -31,6 +29,12 @@ class python {
       "ipython":
       command => "/usr/bin/sudo pip install ipython",
       require => Package["python-dev", "python-pip"]
+    }
+
+    exec {
+      "requests":
+      command => "/usr/bin/sudo pip install requests",
+      require => Package["python", "python-pip"],
     }
 
 }
@@ -49,17 +53,73 @@ class pythonxml {
 	}
 }
 
-class xmlcli {
-  #command-line exes for xml processing
+class saxonb {
+  #command-line exes for xml processing (saxonb for xslt2 
+  #transforms, xerces for pparse/stdinparse validation)
   package { 
       [ "libsaxonb-java", "default-jre" ]:
         ensure => ["installed"],
         require => Exec['apt-update']   
     }
+}
+
+class install_xerces {
+    #wget http://www.trieuvan.com/apache//xerces/c/3/binaries/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz
+    #gzip -d xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz
+    #tar -xf xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar
+    #export PATH="$PATH:/home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4/bin"
+    #export LD_LIBRARY_PATH=/home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4/lib:$LD_LIBRARY_PATH
+    
+    Exec['wget gz'] -> File['xerces gz'] -> Exec["extract"] -> Exec["move xerces"] -> 
+    Exec["export path"] -> Exec["export ld_path"] -> Exec["rm gz"]
+
+    exec {
+      "wget gz":
+      path => "/home/vagrant",
+      command => "/usr/bin/sudo /usr/bin/wget http://www.trieuvan.com/apache//xerces/c/3/binaries/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz",
+      require => Exec['apt-update']
+    }
+
+    file {
+      "xerces gz":
+      path => "/home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz"
+    }
+
+    exec {
+      "extract":
+      path => "/bin:/usr/bin",
+      command => "sudo tar -xzf /home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz",
+      creates => "/home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4"
+    }
+
+    exec {
+      "move xerces":
+      command => "/usr/bin/sudo mv /home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4 /usr/bin/xerces",
+      creates => "/usr/bin/xerces"
+    }
+
+    exec {
+      "export path":
+      command => 'export PATH="$PATH:/usr/bin/xerces/bin"'
+    }
+
+    exec {
+      "export ld_path":
+      command => 'LD_LIBRARY_PATH=/usr/bin/xerces/lib:$LD_LIBRARY_PATH'
+    }
+
+    exec {
+      "rm gz":
+      command => "/usr/bin/sudo rm /home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz"
+    }
 
 }
 
 include core
-include python
-include pythonxml
-include xmlcli
+#include python
+#include pythonxml
+include saxonb
+include install_xerces
+
+
+
