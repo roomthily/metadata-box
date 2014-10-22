c6692f15edb5971b2888a7215be22a2df6135a0b
roomthily <soren.scott@nsidc.org>
2014-10-22 10:06:08 -0600

    comments for however you want to build xerces

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ metadata-box
 ============
 
 vagrant set-up for a basic python box for xml metadata processing
+
+To check the xerces install (just to check on things), run:
+
+```
+PParse -v=always -n -s /vagrant/metadata/raster_ISO-19115-2003.xml
+```
+
+It may not validate for whatever reason but PParse should at least execute.
--- manifests/init.pp
+++ manifests/init.pp
@@ -78,45 +78,46 @@ class install_xerces {
     #export PATH="$PATH:/home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4/bin"
     #export LD_LIBRARY_PATH=/home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4/lib:$LD_LIBRARY_PATH
 
-    Exec['wget gz'] -> File['xerces gz'] -> Exec["extract"] -> Exec["move xerces"] -> Exec["rm gz"]
+    # to go this route, uncomment through the etc profile block
+    # Exec['wget gz'] -> File['xerces gz'] -> Exec["extract"] -> Exec["move xerces"] -> Exec["rm gz"]
 
-    exec {
-      "wget gz":
-      path => "/home/vagrant",
-      command => "/usr/bin/sudo /usr/bin/wget http://www.trieuvan.com/apache//xerces/c/3/binaries/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz",
-      require => Exec['apt-update']
-    }
+    # exec {
+    #   "wget gz":
+    #   path => "/home/vagrant",
+    #   command => "/usr/bin/sudo /usr/bin/wget http://www.trieuvan.com/apache//xerces/c/3/binaries/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz",
+    #   require => Exec['apt-update']
+    # }
 
-    file {
-      "xerces gz":
-      path => "/home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz"
-    }
+    # file {
+    #   "xerces gz":
+    #   path => "/home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz"
+    # }
 
-    exec {
-      "extract":
-      path => "/bin:/usr/bin",
-      command => "sudo tar -xzf /home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz",
-      creates => "/home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4"
-    }
+    # exec {
+    #   "extract":
+    #   path => "/bin:/usr/bin",
+    #   command => "sudo tar -xzf /home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz",
+    #   creates => "/home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4"
+    # }
 
-    exec {
-      "move xerces":
-      command => "/usr/bin/sudo mv /home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4 /usr/bin/xerces",
-      creates => "/usr/bin/xerces"
-    }
+    # exec {
+    #   "move xerces":
+    #   command => "/usr/bin/sudo mv /home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4 /usr/bin/xerces",
+    #   creates => "/usr/bin/xerces"
+    # }
 
-    exec {
-      "rm gz":
-      path => "/bin:/usr/bin",
-      command => "/usr/bin/sudo rm /home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz"
-    }
+    # exec {
+    #   "rm gz":
+    #   path => "/bin:/usr/bin",
+    #   command => "/usr/bin/sudo rm /home/vagrant/xerces-c-3.1.1-x86_64-linux-gcc-3.4.tar.gz"
+    # }
 
-    file {
-      "/etc/profile.d/append_xerces.sh":
-      mode => 644,
-      content => "export PATH=\"\${PATH}:/usr/bin/xerces/bin\"; export LD_LIBRARY_PATH=\"/usr/bin/xerces/lib:\${LD_LIBRARY_PATH}\"",
-      require => Exec["rm gz"]
-    }
+    # file {
+    #   "/etc/profile.d/append_xerces.sh":
+    #   mode => 644,
+    #   content => "export PATH=\"\${PATH}:/usr/bin/xerces/bin\"; export LD_LIBRARY_PATH=\"/usr/bin/xerces/lib:\${LD_LIBRARY_PATH}\"",
+    #   require => Exec["rm gz"]
+    # }
 }
 
 include core
